beec4046021da44efe0c4189700ffaa09d5d5c34
Olivier Fauchon <ofauchon2204@gmail.com>
2018-12-21 00:26:52 +0100

    LED OFF at boot

--- mote-devboard/kw2xd/demo-riot/boards/oflmote-dev/board.c
+++ mote-devboard/kw2xd/demo-riot/boards/oflmote-dev/board.c
@@ -58,9 +58,9 @@ void board_init(void)
 {
     /* initialize the on-board LEDs */
     gpio_init(LED0_PIN, GPIO_OUT);
-    gpio_set(LED0_PIN);
+    gpio_clear(LED0_PIN);
     gpio_init(LED1_PIN, GPIO_OUT);
-    gpio_set(LED1_PIN);
+    gpio_clear(LED1_PIN);
 
     // Safeguard: Infinite loop if board started with buttons pushed
     // This way, we avoid bricking the device (bad pin init, low power modes and no jtag...) 
